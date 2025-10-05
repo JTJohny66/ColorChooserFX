9b8a8f3f3886f4291ff2fbfab7d89e931920db4e
Jonathan Williams <JTW663@gmail.com>
2025-10-05 19:38:41 -0400

    name change

--- src/main/java/com/example/colorchooserapp/ColorApplication.java
+++ src/main/java/com/example/colorchooserapp/ColorApplication.java
@@ -12,7 +12,7 @@ public class ColorApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ColorApplication.class.getResource("ColorChooser.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 400);
-        stage.setTitle("Color Chooser");
+        stage.setTitle("Color Chooser 2");
         stage.setScene(scene);
         stage.show();
     }
